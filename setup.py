113cd8109e252a43c5c02c61f2b7122ef179d77b
skj <shivkrishnajaiswal@gmail.com>
2018-11-07 12:23:57 +0530

    adding requirement.txt and setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='arduino',
+    version='0.1',
+    packages=['light_blinker'],
+    url='https://github.com/ShivKJ/arduino',
+    license='open source',
+    author='Shiv',
+    author_email='shivkj001@gmail.com',
+    description='basics'
+)
